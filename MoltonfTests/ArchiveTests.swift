47439a201eff753cc008f74e7a29b3443a782ea9
Hironori Ichimiya <hiron@hironytic.com>
2016-06-04 09:10:12 +0900

    Test case for VillageElementConverter

--- MoltonfTests/ArchiveTests.swift
+++ MoltonfTests/ArchiveTests.swift
@@ -24,7 +24,19 @@
 //
 
 import XCTest
+import XMLPullitic
 @testable import Moltonf
+
+private class MockJSONWriter: ArchiveJSONWriter {
+    var output: [(fileName: String, object: [String: AnyObject])] = []
+    func writeArchiveJSON(fileName fileName: String, object: [String: AnyObject]) throws {
+        output.append((fileName: fileName, object: object))
+    }
+}
+
+private enum TestError: ErrorType {
+    case CreateParser
+}
 
 class ArchiveTests: XCTestCase {
     var outDir: String = ""
@@ -62,17 +74,75 @@ class ArchiveTests: XCTestCase {
         
         let landName = playdata?[ArchiveKeys.LAND_NAME] as? String
         XCTAssertEqual(landName, "人狼BBS:F国")
-        
-        let vid = playdata?[ArchiveKeys.VID] as? Int
-        XCTAssertEqual(vid, 0)
-        
-        let isValid = playdata?[ArchiveKeys.IS_VALID] as? Bool
-        XCTAssertEqual(isValid, true)
-        
-        let avatarList = playdata?[ArchiveKeys.AVATAR_LIST] as? [[String: AnyObject]]
-        XCTAssertNotNil(avatarList)
-        let avatar0 = avatarList?[0]
-        let avatar0Id = avatar0?[ArchiveKeys.AVATAR_ID] as? String
-        XCTAssertEqual(avatar0Id, "gerd")
+    }
+    
+    func setupTargetElement(xml: String) throws -> (parser: XMLPullParser, element: XMLElement) {
+        guard let parser = XMLPullParser(string: xml) else {
+            throw TestError.CreateParser
+        }
+        parser.shouldProcessNamespaces = true
+
+        while true {
+            switch try parser.next() {
+            case .StartElement(name: _, namespaceURI: _, element: let element):
+                return (parser: parser, element: element)
+            default:
+                break
+            }
+        }
+    }
+    
+    func testConvertVillage() {
+        do {
+            let (parser, element) = try setupTargetElement(
+                "<village\n" +
+                "  xmlns=\"http://jindolf.sourceforge.jp/xml/ns/401\"\n" +
+                "  xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n" +
+                "  xmlns:xlink=\"http://www.w3.org/1999/xlink\"\n" +
+                "  xsi:schemaLocation=\"http://jindolf.sourceforge.jp/xml/ns/401 http://jindolf.sourceforge.jp/xml/xsd/bbsArchive-091001.xsd\"\n" +
+                "  xml:lang=\"ja-JP\"\n" +
+                "  xml:base=\"http://192.168.150.129/moltonf/\"\n" +
+                "  fullName=\"F0 テスト用の村\" vid=\"0\"\n" +
+                "  commitTime=\"08:15:00+09:00\"\n" +
+                "  state=\"gameover\" isValid=\"true\"\n" +
+                "  landName=\"人狼BBS:F国\" formalName=\"人狼BBS:F\"\n" +
+                "  landId=\"wolff\" landPrefix=\"F\"\n" +
+                "  locale=\"ja-JP\" origencoding=\"Shift_JIS\" timezone=\"GMT+09:00\"\n" +
+                "  graveIconURI=\"plugin_wolf/img/face99.jpg\"\n" +
+                "  generator=\"nobody1.0\"\n" +
+                ">\n" +
+                "  <avatarList>\n" +
+                "    <avatar\n" +
+                "      avatarId=\"gerd\"\n" +
+                "      fullName=\"楽天家 ゲルト\" shortName=\"ゲルト\"\n" +
+                "      faceIconURI=\"plugin_wolf/img/face01.jpg\"\n" +
+                "    />\n" +
+                "  </avatarList>\n" +
+                "</village>\n"
+            )
+            
+            let writer = MockJSONWriter()
+            try ArchiveToJSON.VillageElementConverter(parser: parser).convert(element, writer: writer)
+            
+            let output = writer.output[0]
+            let playdata = output.object
+
+            XCTAssertEqual(output.fileName, "playdata.json")
+            
+            let landName = playdata[ArchiveKeys.LAND_NAME] as? String
+            XCTAssertEqual(landName, "人狼BBS:F国")
+            
+            let vid = playdata[ArchiveKeys.VID] as? Int
+            XCTAssertEqual(vid, 0)
+            
+            let isValid = playdata[ArchiveKeys.IS_VALID] as? Bool
+            XCTAssertEqual(isValid, true)
+            
+            let avatarList = playdata[ArchiveKeys.AVATAR_LIST] as? [[String: AnyObject]]
+            XCTAssertNotNil(avatarList)
+        } catch let error {
+            XCTFail("error: \(error)")
+            return
+        }
     }
 }
